ba0fd4915ea559ac8fff39bb6b0029f37235c09b
Darren Clift <cobra3282000@live.com>
2024-09-23 10:41:54 -0700

    config files

---
+++ usr/shared/cinnamon-session/config.py
@@ -0,0 +1,7 @@
+# Generated file - DO NOT EDIT.  Edit config.py.in instead.
+
+LOCALE_DIR="/usr/share/locale"
+PACKAGE="cinnamon-session"
+VERSION="6.2.1"
+DBUS_ADDRESS="unix:abstract=cinnamon-session-quit-dialog"
+PKG_DATADIR="/usr/share/cinnamon-session"
